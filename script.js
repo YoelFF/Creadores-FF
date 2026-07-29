930b61de46450c0d697b904aaa714cb623e0423f
YoelFF <yoelbrauton0@gmail.com>
2026-07-29 11:07:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const boton = document.getElementById("registrar");
+
+boton.addEventListener("click", () => {
+    alert("🔥 Gracias por registrarte en Creadores FF. Pronto recibirás más información.");
+});
